dbe5cb51e6e02935309d9cf3b032fc12fc1d5d69
lkjjjang <lkjjjang@naver.com>
2023-04-10 12:47:51 +0900

    first commit

---
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,38 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 이상 되도록 넣어주세요")
+public class Item {
+
+//    @NotNull(groups = UpdateCheck.class)
+      private Long id;
+
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @NotBlank
+    private String itemName;
+
+//    @NotNull@NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
+    private Integer price;
+
+//    @NotNull@NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
